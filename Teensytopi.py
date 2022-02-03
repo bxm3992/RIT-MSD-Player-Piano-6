913e9666a5cdb85e815e2bbb92f70b523258c4b3
bxm3992 <bxm3992@g.rit.edu>
2022-02-03 12:17:48 -0500

    trying another method

--- Teensytopi.py
+++ Teensytopi.py
@@ -1,98 +1,27 @@
-import serial
-import time
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# lsusb to check device name
+#dmesg | grep "tty" to find port name
 
-startMarker = '<'
-endMarker = '>'
-dataStarted = False
-dataBuf = ""
-messageComplete = False
+import serial,time
 
-#========================
-#========================
-    # the functions
 
-def setupSerial(baudRate, serialPortName):
+if __name__ == '__main__':
     
-    global  serialPort
-    
-    serialPort = serial.Serial(port= serialPortName, baudrate = baudRate, timeout=0, rtscts=True)
-
-    print("Serial port " + serialPortName + " opened  Baudrate " + str(baudRate))
-
-    waitForArduino()
-
-#========================
-
-def sendToArduino(stringToSend):
-    
-        # this adds the start- and end-markers before sending
-    global startMarker, endMarker, serialPort
-    
-    stringWithMarkers = (startMarker)
-    stringWithMarkers += stringToSend
-    stringWithMarkers += (endMarker)
-
-    serialPort.write(stringWithMarkers.encode('utf-8')) # encode needed for Python3
-
-
-#==================
-
-def recvLikeArduino():
-
-    global startMarker, endMarker, serialPort, dataStarted, dataBuf, messageComplete
-
-    if serialPort.inWaiting() > 0 and messageComplete == False:
-        x = serialPort.read().decode("utf-8") # decode needed for Python3
-        
-        if dataStarted == True:
-            if x != endMarker:
-                dataBuf = dataBuf + x
-            else:
-                dataStarted = False
-                messageComplete = True
-        elif x == startMarker:
-            dataBuf = ''
-            dataStarted = True
-    
-    if (messageComplete == True):
-        messageComplete = False
-        return dataBuf
-    else:
-        return "XXX" 
-
-#==================
-
-def waitForArduino():
-
-    # wait until the Arduino sends 'Arduino is ready' - allows time for Arduino reset
-    # it also ensures that any bytes left over from a previous message are discarded
-    
-    print("Waiting for Arduino to reset")
-     
-    msg = ""
-    while msg.find("Arduino is ready") == -1:
-        msg = recvLikeArduino()
-        if not (msg == 'XXX'): 
-            print(msg)
-
-
-
-#====================
-#====================
-    # the program
-
-
-setupSerial(115200, "/dev/ttyACM0")
-count = 0
-prevTime = time.time()
-while True:
-            # check for a reply
-    arduinoReply = recvLikeArduino()
-    if not (arduinoReply == 'XXX'):
-        print ("Time %s  Reply %s" %(time.time(), arduinoReply))
-        
-        # send a message at intervals
-    if time.time() - prevTime > 1.0:
-        sendToArduino("this is a test " + str(count))
-        prevTime = time.time()
-        count += 1
+    print('Running. Press CTRL-C to exit.')
+    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+        time.sleep(0.1) #wait for serial to open
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    cmd=input("Enter command : ")
+                    arduino.write(cmd.encode())
+                    #time.sleep(0.1) #wait for arduino to answer
+                    while arduino.inWaiting()==0: pass
+                    if  arduino.inWaiting()>0: 
+                        answer=arduino.readline()
+                        print(answer)
+                        arduino.flushInput() #remove data after reading
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
