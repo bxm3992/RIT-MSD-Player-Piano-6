d1fe6135f555d2630dc031ab58633dfa903d275f
bxm3992 <bxm3992@g.rit.edu>
2022-02-08 12:19:52 -0500

    Update Teensytopi.py

--- Teensytopi.py
+++ Teensytopi.py
@@ -14,10 +14,15 @@ if __name__ == '__main__':
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
             try:
+                cmd=input("Enter command : ")
+                arduino.write(cmd.encode())
+                time.sleep(0.1) #wait for arduino to answer
+                while arduino.inWaiting()==0:
+                    if  arduino.inWaiting()>0: 
+                        answer=arduino.read()
+                        print(answer)
+                        arduino.flushInput() #remove data after reading
                 while True:
-                    cmd="start"        #input("Enter command : ")
-                    arduino.write(cmd.encode())
-                    time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting()==0: pass
                     if  arduino.inWaiting()>0: 
                         answer=arduino.read()
