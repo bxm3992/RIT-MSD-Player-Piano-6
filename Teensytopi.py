d84eef42e7a8c2e1c3133b4c3a10b8b709600420
bxm3992 <bxm3992@g.rit.edu>
2022-02-08 12:15:57 -0500

    Update Teensytopi.py

--- Teensytopi.py
+++ Teensytopi.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
             print("{} connected!".format(arduino.port))
             try:
                 while True:
-                    #cmd=input("Enter command : ")
-                    #arduino.write(cmd.encode())
+                    cmd="start"        #input("Enter command : ")
+                    arduino.write(cmd.encode())
                     time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting()==0: pass
                     if  arduino.inWaiting()>0: 
