f5aa68d666816c6366db1dc263e73524857b3ee4
bxm3992 <bxm3992@g.rit.edu>
2022-02-24 13:46:05 -0500

    Update TeensyMiditoPi.py
    updated to new version

--- TeensyMiditoPi.py
+++ TeensyMiditoPi.py
@@ -3,34 +3,22 @@
 # lsusb to check device name
 #dmesg | grep "tty" to find port name
 
-import serial,time
-import mido
+import serial
+import mido, time
 
 
 if __name__ == '__main__':
     
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+    with serial.Serial("/dev/ttyACM0", 31250, timeout=1) as arduino:
         time.sleep(0.1) #wait for serial to open
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    
-                    time.sleep(0.1) #wait for arduino to answer
-                    while arduino.inWaiting()==0: pass
-                    if  arduino.inWaiting()>0: 
-                        answer=arduino.read(4)
-                        print(answer[0],",",answer[1],",",answer[2],",",answer[3])
-                        arduino.flushInput() #remove data after reading
-
-                    # cmd=input("Enter command : ")
-                    # arduino.write(cmd.encode())
-                    # #time.sleep(0.1) #wait for arduino to answer
-                    # while arduino.inWaiting()==0: pass
-                    # if  arduino.inWaiting()>0: 
-                    #     answer=arduino.readline()
-                    #     print(answer)
-                    #     arduino.flushInput() #remove data after reading
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+            arduino.flush()
+            # Infinite loop
+            while (1):
+            # If there is data available
+                if(arduino.in_waiting > 0):
+                    line = arduino.readline().decode('utf-8').rstrip()
+                    # Print the data received from the Arduino
+                    print(line)
