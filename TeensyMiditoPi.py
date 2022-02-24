381f635cf5cb9b39992a4d889f80a31fc92d4221
bxm3992 <bxm3992@g.rit.edu>
2022-02-24 13:52:38 -0500

    Update TeensyMiditoPi.py
    update to original

--- TeensyMiditoPi.py
+++ TeensyMiditoPi.py
@@ -9,16 +9,18 @@ import mido, time
 
 if __name__ == '__main__':
     
-    print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 31250, timeout=1) as arduino:
+    #print('Running. Press CTRL-C to exit.')
+    #with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+    #if arduino.isOpen():
+            #print("{} connected!".format(arduino.port))
+            
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         time.sleep(0.1) #wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            arduino.flush()
-            # Infinite loop
-            while (1):
+        ser.flush()
+        # Infinite loop
+        while (1):
             # If there is data available
-                if(arduino.in_waiting > 0):
-                    line = arduino.readline().decode('utf-8').rstrip()
+                if(ser.in_waiting > 0):
+                    line = ser.readline().decode('utf-8').rstrip()
                     # Print the data received from the Arduino
                     print(line)
