b339c3b3ac1e181b57b4cdbc20452476b72af733
bxm3992 <bxm3992@g.rit.edu>
2022-02-28 18:24:52 -0500

    Update Teensytopi2.0.py
    updating for array

--- Teensytopi2.0.py
+++ Teensytopi2.0.py
@@ -20,8 +20,10 @@ if __name__ == '__main__':
         
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
+    data_length=4
     while True:
         if ser.in_waiting > 0:
-            line = ser.read(4)
-            print("OUTPUT: " + line)
-            ser.flush()
+            data = ser.read(size=data_length)
+            for i in range(data_length):
+                print(data[i]) 
+            #ser.flush()
