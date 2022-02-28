c71d05b62e1053b11eb24147dba21cda6c21e2c9
bxm3992 <bxm3992@g.rit.edu>
2022-02-28 15:41:40 -0500

    Update TeensyMiditoPi.py
    trying to delay for sys

--- TeensyMiditoPi.py
+++ TeensyMiditoPi.py
@@ -14,13 +14,17 @@ if __name__ == '__main__':
     #with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
     #if arduino.isOpen():
             #print("{} connected!".format(arduino.port))
+    
+    time.sleep(.5)
     try:
         ser = serial.Serial("/dev/ttyACM0", 9600,timeout=0, parity=serial.PARITY_NONE, 
                         stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
     except:
         sys.exit("Error connecting device")        
     #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    time.sleep(.5)
     ser.reset_input_buffer()
+    time.sleep(.01)
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
