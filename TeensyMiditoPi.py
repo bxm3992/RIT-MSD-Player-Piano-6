67944528631cbf10f1c73123f5a97dc0423b848c
bxm3992 <bxm3992@g.rit.edu>
2022-02-28 17:37:22 -0500

    Update TeensyMiditoPi.py

--- TeensyMiditoPi.py
+++ TeensyMiditoPi.py
@@ -23,4 +23,5 @@ if __name__ == '__main__':
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
+            print("OUTPUT: ")
             print(line)
